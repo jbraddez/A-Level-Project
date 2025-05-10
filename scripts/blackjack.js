a1d49d34beed5eedf7f2c78a83c5d543837b726f
jbradleydev <jacobbradleydev@gmail.com>
2025-05-10 18:07:43 +0100

    blackjack + diff tables finished

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -15,15 +15,86 @@ let canStart = false;
 const startBtn = document.getElementById('start');
 const betsCont = document.getElementById('betsCont');
 const betAmountEl = document.getElementById('betAmount');
+const tableSelect = document.getElementById('table');
+let tableMax = 10000;
+let tableMin = 0;
+let belowMinBet = false;
+
+const chipRow1 = document.getElementById('chipRow1');
+const chipRow2 = document.getElementById('chipRow2');
+tableSelect.addEventListener('change',(event)=>{
+    resetGame();
+    tableMax = parseInt(event.target.value);
+    const selectedOption = document.querySelector(`#table option[value="${tableMax}"]`);
+    tableMin = parseInt(selectedOption.getAttribute('data-min'));
+    document.body.classList.remove('table-10k', 'table-50k', 'table-100k', 'table-250k', 'oneB-table');
+
+    if(tableMax == 10000){
+        chipRow1.innerHTML=`<button onclick="addBet(100)"><img src="/images/chips/100.png" alt="100"></button>
+            <button onclick="addBet(250)"><img src="/images/chips/250.png" alt="250"></button>
+            <button onclick="addBet(500)"><img src="/images/chips/500.png" alt="500"></button>`;
+        chipRow2.innerHTML=`<button onclick="addBet(1000)"><img src="/images/chips/1000.png" alt="1000"></button>
+            <button onclick="addBet(2500)"><img src="/images/chips/2500.png" alt="2500"></button>
+            <button onclick="addBet(5000)"><img src="/images/chips/5000.png" alt="5000"></button>`;
+        //add bg style
+        document.body.classList.add('table-10k');
+    }else if(tableMax == 50000){
+        chipRow1.innerHTML=`<button onclick="addBet(1000)"><img src="/images/chips/1000.png" alt="1000"></button>
+            <button onclick="addBet(2500)"><img src="/images/chips/2500.png" alt="2500"></button>
+            <button onclick="addBet(5000)"><img src="/images/chips/5000.png" alt="5000"></button>`;
+        chipRow2.innerHTML=`<button onclick="addBet(10000)"><img src="/images/chips/10000.png" alt="10000"></button>
+            <button onclick="addBet(20000)"><img src="/images/chips/20000.png" alt="20000"></button>
+            <button onclick="addBet(50000)"><img src="/images/chips/50000.png" alt="50000"></button>`;
+        //add bg style
+        document.body.classList.add('table-50k');
+
+    }else if(tableMax == 100000){
+        chipRow1.innerHTML=`<button onclick="addBet(1000)"><img src="/images/chips/1000.png" alt="1000"></button>
+            <button onclick="addBet(2500)"><img src="/images/chips/2500.png" alt="2500"></button>
+            <button onclick="addBet(5000)"><img src="/images/chips/5000.png" alt="5000"></button>`;
+        chipRow2.innerHTML=`<button onclick="addBet(10000)"><img src="/images/chips/10000.png" alt="10000"></button>
+            <button onclick="addBet(20000)"><img src="/images/chips/20000.png" alt="20000"></button>
+            <button onclick="addBet(50000)"><img src="/images/chips/50000.png" alt="50000"></button>`;
+        //add bg style
+        document.body.classList.add('table-100k');
+
+    }else if(tableMax == 250000){
+        chipRow1.innerHTML=`<button onclick="addBet(10000)"><img src="/images/chips/10000.png" alt="10000"></button>
+            <button onclick="addBet(20000)"><img src="/images/chips/20000.png" alt="20000"></button>
+            <button onclick="addBet(50000)"><img src="/images/chips/50000.png" alt="50000"></button>`;
+        chipRow2.innerHTML=`<button onclick="addBet(75000)"><img src="/images/chips/75000.png" alt="75000"></button>
+            <button onclick="addBet(100000)"><img src="/images/chips/100000.png" alt="100000"></button>
+            <button onclick="addBet(200000)"><img src="/images/chips/200000.png" alt="200000"></button>`;
+        //add bg style
+        document.body.classList.add('table-250k');
+    }
+    //1 billion only table
+    if(tableMax == 0){
+        chipRow1.innerHTML=`<button onclick="addBet(1000000000)"><img src="/images/chips/1000000000.png" alt="1000000000"></button>`;
+        chipRow2.style.display = 'none';
+        document.body.classList.add('oneB-table');
+    }else{
+        chipRow2.style.display = 'flex';
+    }
+})
+
+//dispath event for selected state due to content not changing when history back to this page
+window.addEventListener('pageshow', () => {
+    tableSelect.dispatchEvent(new Event('change'));
+});
+
+
 function addBet(betToAdd){
-    if(bet+betToAdd > 10000){
-        showNotification('Maximum Bet Reached',"This chip can't be added, the maximum bet is 10,000",'red');
+    if(bet+betToAdd > tableMax && tableMax != 0){
+        showNotification('Maximum Bet Reached',`This chip can't be added, the maximum bet is ${tableMax.toLocaleString()}`,'red');
         return;
     }
     if(bet+betToAdd > getChips()){
         showNotification('Maximum Bet Reached',"You do not have sufficient chips for this bet.",'red');
         return;
     }
+    
+
     bet+=betToAdd;
     betAmountEl.textContent = bet;
 
@@ -50,6 +121,10 @@ const dealerBox = document.getElementById('dealerBox');
 //start
 function start(){
     if(!canStart){
+        return;
+    }
+    if(bet < tableMin){
+        showNotification('Please Bet More',`The minimum bet is ${tableMin.toLocaleString()}`,'red');
         return;
     }
     playerValueEl.style.display = 'block';
